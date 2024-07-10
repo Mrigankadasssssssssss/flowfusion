a2268a5ff9f9f69cc4572585c755efb44de2f9a9
Mriganka Das <dasmriganka.2002@gmail.com>
2024-07-10 22:34:09 +0530

    features : lanfing page and dashboard side bar is implemented
    powered-by : Mriganka Das
    
    <dasmriganka.2002@gmail.com>

---
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DashBoardPage = () => {
+  return (
+    <div className="flex flex-col gap-4 relative">
+      <h1 className="text-4xl sticky top-0 flex p-6 z-10 items-center border-b bg-background/50 backdrop-blur-lg">
+        DashBoard
+      </h1>
+    </div>
+  );
+};
+
+export default DashBoardPage;
